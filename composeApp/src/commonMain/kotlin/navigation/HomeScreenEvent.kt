a46c8635c4574a9d8cf5c184977d0d5fa19ffece
ankitdutta170 <ankitdutta170@gmail.com>
2024-03-31 10:54:01 +0530

    kmm_news_app

--- composeApp/src/commonMain/kotlin/navigation/HomeScreenEvent.kt
+++ composeApp/src/commonMain/kotlin/navigation/HomeScreenEvent.kt
@@ -4,6 +4,8 @@ import network.model.Article
 
 sealed interface HomeScreenEvent {
 
+    data object OnBackClicked : HomeScreenEvent
+
     data class OnNewsItemClicked(val article: Article):HomeScreenEvent
 
     data class OnUpdateArticle(val article: Article):HomeScreenEvent
--- composeApp/src/commonMain/kotlin/navigation/NewsDetailScreenComponentt.kt
+++ composeApp/src/commonMain/kotlin/navigation/NewsDetailScreenComponentt.kt
@@ -1,10 +1,24 @@
 package navigation
 
+import androidx.compose.runtime.mutableStateOf
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.router.stack.StackNavigation
+import com.arkivanov.decompose.router.stack.pop
 import network.model.Article
 
 class NewsDetailScreenComponentt(
     val article: Article,
+    private val navigation: StackNavigation<RootComponent.Configuration>,
     componentContext: ComponentContext)
     :ComponentContext by componentContext{
+
+    val state = mutableStateOf(article)
+
+    fun onEvent(event: NewsDetailsUiEvent) {
+        when (event) {
+            is NewsDetailsUiEvent.OnBackClicked -> {
+                navigation.pop()
+            }
+        }
+    }
 }
